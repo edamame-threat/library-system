51a3c9fd306fccbeac54b1c0d641b99b7244afa1
Sofia Babyshkina <150817591+edamame-threat@users.noreply.github.com>
2024-01-12 15:24:29 +0700

    Create Author.java

---
+++ Author.java
@@ -0,0 +1,18 @@
+// Author.java
+public class Author {
+    private String name;
+    private int age;
+
+    public Author(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
